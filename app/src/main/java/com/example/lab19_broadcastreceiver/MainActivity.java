9256ccb1f2dd6912308a7883275963e6e443df5d
Hash-himadri <hashcodes324@gmail.com>
2023-02-18 17:05:56 +0530

    initial commit

---
+++ app/src/main/java/com/example/lab19_broadcastreceiver/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.lab19_broadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    Switch wifiSwitch;
+    WifiManager wifiManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wifiSwitch = findViewById(R.id.wifiSwitch);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    wifiManager.setWifiEnabled(true);
+                    wifiSwitch.setText("Wifi is ON");
+                }
+                else{
+                    wifiManager.setWifiEnabled(false);
+                    wifiSwitch.setText("Wifi is OFF");
+                }
+            }
+        });
+    }
+
+    private BroadcastReceiver wifiStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int wifiStateExtra = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE,
+                    WifiManager.WIFI_STATE_UNKNOWN);
+
+            switch (wifiStateExtra){
+                case WifiManager.WIFI_STATE_ENABLED:
+                    wifiSwitch.setChecked(true);
+                    wifiSwitch.setText("Wifi is ON");
+                    break;
+
+                case WifiManager.WIFI_STATE_DISABLED:
+                    wifiSwitch.setChecked(false);
+                    wifiSwitch.setText("Wifi is OFF");
+                    break;
+            }
+        }
+    };
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter =
+                new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        registerReceiver(wifiStateReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(wifiStateReceiver);
+    }
+}
